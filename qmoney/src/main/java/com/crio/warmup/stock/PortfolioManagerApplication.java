63ffaf8678e3f62bbaf84d612e9cbf65a8afd730
CrioUser <crio-user@criodo.com>
2021-11-18 16:18:46 +0530

    ME_QMONEY_V2_MODULE_JSON_PARSING

--- qmoney/src/main/java/com/crio/warmup/stock/PortfolioManagerApplication.java
+++ qmoney/src/main/java/com/crio/warmup/stock/PortfolioManagerApplication.java
@@ -9,20 +9,13 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.UUID;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import org.apache.logging.log4j.ThreadContext;
-import org.springframework.web.client.RestTemplate;
 
 
 public class PortfolioManagerApplication {
@@ -45,8 +38,18 @@ public class PortfolioManagerApplication {
   //  2. You can use "./gradlew build" to check if your code builds successfully.
 
   public static List<String> mainReadFile(String[] args) throws IOException, URISyntaxException {
+    
+    File filepath = resolveFileFromResources(args[0]);
+    ObjectMapper objectmapper = getObjectMapper();
+    PortfolioTrade [] portfolioTrade = objectmapper.readValue(filepath, PortfolioTrade[].class);
+    List <String> resultList = new ArrayList<>();
+    for(PortfolioTrade trade : portfolioTrade){
+      resultList.add(trade.getSymbol());
+      //System.out.println(trade);
+    } 
+    return resultList;
 
-     return Collections.emptyList();
+    // return Collections.emptyList();
   }
 
 
@@ -67,6 +70,8 @@ public class PortfolioManagerApplication {
   //    and deserialize the results in List<Candle>
 
 
+
+ 
 
   private static void printJsonObject(Object object) throws IOException {
     Logger logger = Logger.getLogger(PortfolioManagerApplication.class.getCanonicalName());
@@ -117,7 +122,8 @@ public class PortfolioManagerApplication {
   public static List<String> debugOutputs() {
 
      String valueOfArgument0 = "trades.json";
-     String resultOfResolveFilePathArgs0 = "";
+     
+     String resultOfResolveFilePathArgs0 = "args[0]";
      String toStringOfObjectMapper = "";
      String functionNameFromTestFileInStackTrace = "";
      String lineNumberFromTestFileInStackTrace = "";
